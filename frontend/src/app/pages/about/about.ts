edcb05153852f5d0b7827f6c86c2543f4856f0f9
Zcyde <eldrinjoshpineda@gmail.com>
2026-02-15 23:55:18 +0800

    Optimized home, index, blog, about pages and css

--- frontend/src/app/pages/about/about.ts
+++ frontend/src/app/pages/about/about.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [RouterLink],
+  imports: [RouterLink, NgOptimizedImage],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
--- frontend/src/app/pages/blog-detail/blog-detail.ts
+++ frontend/src/app/pages/blog-detail/blog-detail.ts
@@ -28,17 +28,16 @@ export class BlogDetail implements OnInit {
           next: (data) => {
             this.post = data;
 
-            // --- ADD THIS TO FIX THE LCP DISCOVERY DELAY ---
-            if (this.post && this.post.image) {
-              const link = document.createElement('link');
-              link.rel = 'preload';
-              link.as = 'image';
-              link.href = this.post.image;
-              link.fetchPriority = 'high'; // Prioritizes this over other assets
-              document.head.appendChild(link);
-            }
+            // Fix for LCP Discovery: Create a hidden preload link immediately
+              if (this.post && this.post.image) {
+                const link = document.createElement('link');
+                link.rel = 'preload';
+                link.as = 'image';
+                link.href = this.post.image;
+                link.fetchPriority = 'high';
+                document.head.appendChild(link);
+              }
             // -----------------------------------------------
-
             this.cdr.detectChanges();
           },
           error: (err) => {
