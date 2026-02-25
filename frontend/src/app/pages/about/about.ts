3bfb260ebf0dc88f3fd9c6e8c55cb4f7db6c9406
KGrahhh <keyshrimp@gmail.com>
2026-02-25 12:00:54 +0800

    About Page Update

--- frontend/src/app/pages/about/about.ts
+++ frontend/src/app/pages/about/about.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage, NgFor } from '@angular/common'; 
 import { RouterLink } from '@angular/router';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [RouterLink, NgOptimizedImage],
+  standalone: true,
+  imports: [NgOptimizedImage, NgFor, RouterLink], // Added NgFor here to fix the error
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
 export class About {
+  images: string[] = [
+    'workers.jpg', 
+    'filsafeguy.jpg' ,
+    'filsafeguy2.jpg'
+  ];
+  
+  currentIndex: number = 0;
 
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevImage() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
